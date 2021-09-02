280f5368da1a92260e85bdcec01791de7c12ebff
iram <iram@example.com>
2021-09-01 23:00:19 -0500

    add controller for enemy and bullet prefabs

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,10 +6,24 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 2f;
     public Vector2 direction;
+
+    public float livingTime = 3f;
+    public Color initialColor = Color.white;
+    public Color explosionColor;
+
+    private SpriteRenderer _renderer;
+    private float _startingTime;
+
     // Start is called before the first frame update
+    void Awake()
+    {
+        _renderer = GetComponent<SpriteRenderer>();    
+    }
     void Start()
     {
-        
+        // Save initial time 
+        _startingTime = Time.time;
+        Destroy(this.gameObject, livingTime);
     }
 
     // Update is called once per frame
@@ -17,9 +31,14 @@ public class Bullet : MonoBehaviour
     {
         Vector2 movement = direction.normalized * speed * Time.deltaTime;
 
-        transform.position = new Vector2(transform.position.x + movement.x, transform.position.y + movement.y);
+        //transform.position = new Vector2(transform.position.x + movement.x, transform.position.y + movement.y);
 
         // Son lo mismo
-        //transform.Translate(movement);
+        transform.Translate(movement);
+
+        // Change bullet color over time
+        float _timeSinceStarted = Time.time - _startingTime;
+        float _percentageCompleted = _timeSinceStarted / livingTime;
+        _renderer.color = Color.Lerp(initialColor, explosionColor, _percentageCompleted);
     }
 }
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject bulletPrefab;
+    public GameObject shooter;
+    private Transform _muzzle;
+
+    void Awake()
+    {
+        _muzzle = transform.Find("Muzzle");
+    }
+    void Start()
+    {
+        Invoke("Shoot", 1f);
+        Invoke("Shoot", 2f);
+        Invoke("Shoot", 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        if(bulletPrefab != null && _muzzle != null && shooter )
+        {
+            GameObject myBullet = Instantiate(bulletPrefab, _muzzle.position, Quaternion.identity) as GameObject;
+            Bullet bulletComponent = myBullet.GetComponent<Bullet>();
+
+            if(shooter.transform.localScale.x < 0f)
+            {
+                // Left
+                bulletComponent.direction = Vector2.left; // vectpr(-1x, 0)
+            } else
+            {
+                // Right
+                bulletComponent.direction = Vector2.right; // vectpr(1x, 0)
+            }
+        }
+    }
+}
